722d69e9f93371e7905bfbffc1015b7a0b2bcad7
Gaurav Kaushik <gaurav.kaushik@jasperindia.com>
2013-03-20 13:03:32 +0530

    Added mouse hover metafile

--- src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
+++ src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
@@ -10,6 +10,6 @@ public class SBATFileExecutorClient {
 //		sbatExecutor.execute(new File("src/main/resources/sbat_metafile.txt"));
 //		sbatExecutor.execute(new File("src/main/resources/sbat_text_input_metafile.txt"));
 		sbatExecutor.execute(new File("src/main/resources/sbat_element_click_metafile.txt"));
-		
+//	    sbatExecutor.execute(new File("src/main/resources/sbat_mouse_hover_metafile.txt"));
 	}
 }
