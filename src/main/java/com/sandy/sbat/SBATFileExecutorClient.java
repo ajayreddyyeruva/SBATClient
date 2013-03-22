d9b0d4ec63e236ddd6c17a5288e73d1213f8105f
Gaurav Kaushik <gaurav.kaushik@jasperindia.com>
2013-03-22 14:21:55 +0530

    Added test file for assert text present

--- src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
+++ src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
@@ -12,6 +12,7 @@ public class SBATFileExecutorClient {
 //		sbatExecutor.execute(new File("src/main/resources/sbat_element_click_metafile.txt"));
 //	    sbatExecutor.execute(new File("src/main/resources/sbat_mouse_hover_metafile.txt"));
 //      sbatExecutor.execute(new File("src/main/resources/sbat_complete_test_metafile.txt"));
-		sbatExecutor.execute(new File("src/main/resources/sbat_verify_text_present_metafile.txt"));
+//		sbatExecutor.execute(new File("src/main/resources/sbat_verify_text_present_metafile.txt"));
+	    sbatExecutor.execute(new File("src/main/resources/sbat_assert_text_present_metafile.txt"));
 	}
 }
