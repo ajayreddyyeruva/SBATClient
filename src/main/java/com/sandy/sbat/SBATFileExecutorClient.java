11a3f1a63aaf5c9c940ede4d87e243cc23c66bd2
Gaurav Kaushik <gaurav.kaushik@jasperindia.com>
2013-04-02 17:43:11 +0530

    Added metafiles for verify all images and for verify all links

--- src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
+++ src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
@@ -7,14 +7,6 @@ public class SBATFileExecutorClient {
 	public static void main(String[] args) {
 		SBATExecutor sbatExecutor = new SBATExecutor();
 		
-//		sbatExecutor.execute(new File("src/main/resources/sbat_metafile.txt"));
-//		sbatExecutor.execute(new File("src/main/resources/sbat_text_input_metafile.txt"));
-//		sbatExecutor.execute(new File("src/main/resources/sbat_element_click_metafile.txt"));
-//	    sbatExecutor.execute(new File("src/main/resources/sbat_mouse_hover_metafile.txt"));
-//      sbatExecutor.execute(new File("src/main/resources/sbat_complete_test_metafile.txt"));
-//		sbatExecutor.execute(new File("src/main/resources/sbat_verify_text_present_metafile.txt"));
-//	    sbatExecutor.execute(new File("src/main/resources/sbat_assert_text_present_metafile.txt"));
-//		sbatExecutor.execute(new File("src/main/resources/sbat_verify_element_text_metafile.txt"));
-		sbatExecutor.execute(new File("src/main/resources/sbat_assert_element_text_metafile.txt"));
+		sbatExecutor.execute(new File("src/main/resources/sbat_verify_all_links_metafile.txt"));
 	}
 }
