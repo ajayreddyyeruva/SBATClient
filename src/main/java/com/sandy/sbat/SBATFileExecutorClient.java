1dc00756628529bbb8b2277a2c50968e6940673b
gaurav.kaushik <gaurav.kaushik@jasperindia.com>
2013-04-15 09:57:07 +0530

    rectified names

--- src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
+++ src/main/java/com/sandy/sbat/SBATFileExecutorClient.java
@@ -7,6 +7,6 @@ public class SBATFileExecutorClient {
 	public static void main(String[] args) {
 		SBATExecutor sbatExecutor = new SBATExecutor();
 //		sbatExecutor.execute(new File("src/main/resources/test.txt"));
-		sbatExecutor.execute(new File("src/main/resources/fileUpload.txt"));
+		sbatExecutor.execute(new File("src/main/resources/verifyURL.txt"));
 	}
 }
